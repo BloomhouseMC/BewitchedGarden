5bdbcf7262151531643a1d648676a43ec802083a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-29 15:48:02 -0400

    Herbologist initial

--- src/main/java/dev/mrsterner/witcheskitchen/WitchesKitchenClient.java
+++ src/main/java/dev/mrsterner/witcheskitchen/WitchesKitchenClient.java
@@ -1,10 +1,11 @@
 package dev.mrsterner.witcheskitchen;
 
+import dev.mrsterner.witcheskitchen.common.registry.WKRendering;
 import net.fabricmc.api.ClientModInitializer;
 
 public class WitchesKitchenClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        WKRendering.init();
     }
 }
