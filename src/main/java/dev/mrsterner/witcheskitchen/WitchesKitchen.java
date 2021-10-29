7a3643cdb984a4e05852cff68e1cf6de57ba5054
mrsterner <simon@sterner.dev>
2021-10-29 16:43:25 +0200

    Initial commit

---
+++ src/main/java/dev/mrsterner/witcheskitchen/WitchesKitchen.java
@@ -0,0 +1,20 @@
+package dev.mrsterner.witcheskitchen;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class WitchesKitchen implements ModInitializer {
+	public static final String MODID = "witcheskitchen";
+	public static final ItemGroup WITCHESKITCHEN_GROUP = FabricItemGroupBuilder.build(new Identifier(MODID, MODID), () -> new ItemStack(Items.ACACIA_PLANKS));
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/dev/mrsterner/witcheskitchen/client/dummy.java
@@ -0,0 +1,4 @@
+package dev.mrsterner.witcheskitchen.client;
+
+public class dummy {
+}
