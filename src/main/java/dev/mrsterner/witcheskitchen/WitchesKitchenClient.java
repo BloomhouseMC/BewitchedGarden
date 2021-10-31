1fd688992556698cdcb0bd1d48b94caf9930110d
mrsterner <simon@sterner.dev>
2021-10-31 22:43:34 +0100

    reformat

--- src/main/java/dev/mrsterner/witcheskitchen/WitchesKitchenClient.java
+++ src/main/java/dev/mrsterner/witcheskitchen/WitchesKitchenClient.java
@@ -3,6 +3,7 @@ package dev.mrsterner.witcheskitchen;
 
 import dev.mrsterner.witcheskitchen.client.render.BasePlantEntityRenderer;
 import dev.mrsterner.witcheskitchen.client.renderer.SausageBlockEntityRenderer;
+import dev.mrsterner.witcheskitchen.common.entity.BasePlantEntity;
 import dev.mrsterner.witcheskitchen.common.registry.WKEntities;
 import dev.mrsterner.witcheskitchen.common.registry.WKObjects;
 import dev.mrsterner.witcheskitchen.common.registry.WKRendering;
@@ -16,13 +17,5 @@ public class WitchesKitchenClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
          WKRendering.init();
-
-        BlockEntityRendererRegistry.INSTANCE.register(WKObjects.SAUSAGE_BLOCK_ENTITY, (BlockEntityRendererFactory.Context rendererDispatcherIn) -> new SausageBlockEntityRenderer());
-        //EntityRendererRegistry.INSTANCE.register(WKEntities.VENUS_ENTITY, BasePlantEntityRenderer::new);
-
-
-        for(EntityType entityType : WKEntities.ENTITY_TYPES.keySet()){
-            EntityRendererRegistry.INSTANCE.register(entityType, BasePlantEntityRenderer::new);
-        }
     }
 }
