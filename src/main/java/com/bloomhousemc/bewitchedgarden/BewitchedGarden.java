936e8aefb7f4b2bea282377d96f4dd8f09de8e5c
BuildTools <unconfigured@null.spigotmc.org>
2021-11-07 20:07:12 -0500

    World gen cleanup

--- src/main/java/com/bloomhousemc/bewitchedgarden/BewitchedGarden.java
+++ src/main/java/com/bloomhousemc/bewitchedgarden/BewitchedGarden.java
@@ -1,33 +1,16 @@
 package com.bloomhousemc.bewitchedgarden;
 
-import com.bloomhousemc.bewitchedgarden.common.components.EffigyComponent;
-import com.bloomhousemc.bewitchedgarden.common.entity.EffigyEntity;
-import com.bloomhousemc.bewitchedgarden.common.registry.BGStatusEffects;
 import com.bloomhousemc.bewitchedgarden.common.events.BGEvents;
 import com.bloomhousemc.bewitchedgarden.common.registry.BGEntities;
 import com.bloomhousemc.bewitchedgarden.common.registry.BGObjects;
-import com.bloomhousemc.bewitchedgarden.common.world.BGWorldState;
-import moriyashiine.bewitchment.common.item.TaglockItem;
+import com.bloomhousemc.bewitchedgarden.common.registry.BGStatusEffects;
+import com.bloomhousemc.bewitchedgarden.common.registry.BGWorld;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.fabricmc.fabric.api.event.player.UseEntityCallback;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Pair;
-import net.minecraft.util.registry.Registry;
 import software.bernie.geckolib3.GeckoLib;
-
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
 
 public class BewitchedGarden implements ModInitializer {
 	public static final String MODID = "bewitchedgarden";
@@ -40,5 +23,6 @@ public class BewitchedGarden implements ModInitializer {
 		BGEntities.init();
 		BGEvents.init();
 		BGStatusEffects.init();
+		BGWorld.init();
 	}
 }
--- src/main/java/com/bloomhousemc/bewitchedgarden/common/registry/BGWorld.java
+++ src/main/java/com/bloomhousemc/bewitchedgarden/common/registry/BGWorld.java
@@ -1,18 +1,26 @@
 package com.bloomhousemc.bewitchedgarden.common.registry;
 
 import com.bloomhousemc.bewitchedgarden.BewitchedGarden;
+import com.google.common.collect.ImmutableSet;
+import net.fabricmc.fabric.api.biome.v1.BiomeModification;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
+import net.minecraft.block.Blocks;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.world.gen.feature.ConfiguredFeature;
-import net.minecraft.world.gen.feature.ConfiguredFeatures;
-import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.FeatureConfig;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.*;
+import net.minecraft.world.gen.placer.SimpleBlockPlacer;
+import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 
 public class BGWorld {
 
     // Herbs
-    public static final ConfiguredFeature<?, ?> MINT = register("mint", Feature.RANDOM_PATCH.configure(ConfiguredFeatures.Configs.GRASS_CONFIG).decorate(ConfiguredFeatures.Decorators.SQUARE_HEIGHTMAP_SPREAD_DOUBLE).repeat(5));
+    public static final RandomPatchFeatureConfig MINT_CONFIG = new RandomPatchFeatureConfig.Builder(new SimpleBlockStateProvider(BGObjects.MINT.getDefaultState()), SimpleBlockPlacer.INSTANCE).tries(64).whitelist(ImmutableSet.of(Blocks.GRASS)).cannotProject().build();
+    public static final ConfiguredFeature<?, ?> MINT = register("mint", Feature.RANDOM_PATCH.configure(MINT_CONFIG));
 
     private static <FC extends FeatureConfig> ConfiguredFeature<FC, ?> register(String id, ConfiguredFeature<FC, ?> feature) {
         return Registry.register(BuiltinRegistries.CONFIGURED_FEATURE, new Identifier(BewitchedGarden.MODID, id), feature);
@@ -20,5 +28,8 @@ public class BGWorld {
 
     public static void init() {
 
+        // World Generation
+        BiomeModification world = BiomeModifications.create(new Identifier(BewitchedGarden.MODID, "features"));
+        world.add(ModificationPhase.ADDITIONS, BiomeSelectors.categories(Biome.Category.FOREST), context -> context.getGenerationSettings().addBuiltInFeature(GenerationStep.Feature.VEGETAL_DECORATION, MINT));
     }
 }
